4dc6e2c0c69ef2f1e8617235da67e68c825aedba
eveliis.kallas <eveliis.kallas@khk.ee>
2018-01-18 10:30:24 +0200

    template objekti omadused on määratud

---
+++ model/template.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: eveliis.kallas
+ * Date: 18.01.2018
+ * Time: 10:21
+ */
+
+class template
+{
+    // template objekti/klassi omadused
+    var $file = ''; //html vaade faili nimi
+    //html vaade faili sisu, mis on klassis
+    //vastava funktsiooni abil loetud
+    var $content = false;
+    // reaalsed väärtused html vaade šablooni täitmiseks
+    var $vars = array();
+}
