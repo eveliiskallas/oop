db2a344dde442b17082df0980c196b31a79c15fc
eveliis.kallas <eveliis.kallas@khk.ee>
2018-01-18 11:08:45 +0200

    Konstruktor on kirjeldatud ja pisiparandused on olemas

--- model/template.php
+++ model/template.php
@@ -15,13 +15,23 @@ class template
     var $content = false;
     // reaalsed väärtused html vaade šablooni täitmiseks
     var $vars = array();
+
+    /**
+     * template constructor.
+     * @param string $file
+     */
+    public function __construct($file)
+    {
+        $this->file = $file; // määrame html vaade faili nimi
+        $this->loadFile(); //laadime html vaade faili sisu
+    }
     // template klassi meetodid
     // html vaade faili sisu lugemine
-    function readFile($f){
+    function readFile($file){
 //        $fp = fopen($f, 'rb');
 //        $this->content = fread($fp, filesize($f));
 //        fclose($fp);
-        $this->content = file_get_contents($f);
+        $this->content = file_get_contents($file);
     }
     // html vaade faili kontrollimine
     // ja kasutamisele võtmine
@@ -33,33 +43,34 @@ class template
         }
         // kui html vaade faili nimi antakse kujul:
         // views/test.html
-        $f = $this->file; // abiasendus
-        if(file_exists($f) and is_file($f) and is_readable($f)){
+        $file = $this->file; // abiasendus
+        if(file_exists($file) and is_file($file) and is_readable($file)){
             //loeme sisu failist
-            $this->readFile($f);
+            $this->readFile($file);
         }
         // kui html vaade faili nimi antakse kujul:
         // test.html
-        $f = VIEWS_DIR.$this->file;
-        if(file_exists($f) and is_file($f) and is_readable($f)){
-            $this->readFile($f);
+        $file = VIEWS_DIR.$this->file;
+        if(file_exists($file) and is_file($file) and is_readable($file)){
+            $this->readFile($file);
         }
         // kui html vaade faili nimi antakse kujul:
         // test
-        $f = VIEWS_DIR.$this->file.'.html';
-        if(file_exists($f) and is_file($f) and is_readable($f)) {
+        $file = VIEWS_DIR.$this->file.'.html';
+        if(file_exists($file) and is_file($file) and is_readable($file)) {
             //loeme sisu failist
-            $this->readFile($f);
+            $this->readFile($file);
         }
         // kui html vaade faili nimi antakse kujul:
         // alamkaust.test - views/katsekaust/test.html
-        $f = VIEWS_DIR.str_replace('.', '/', $this->file).'.html';
-        if(file_exists($f) and is_file($f) and is_readable($f)) {
+        $file = VIEWS_DIR.str_replace('.', '/', $this->file).'.html';
+        if(file_exists($file) and is_file($file) and is_readable($file)) {
             //loeme sisu failist
-            $this->readFile($f);
+            $this->readFile($file);
         }
         if($this->content === false){
             echo 'Ei suutnud lugeda faili '.$this->file.'<br />';
         }
     }
+
 }
