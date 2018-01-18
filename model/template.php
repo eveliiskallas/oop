b691c3e527ff783baf5d1e092481b635e30e53d2
eveliis.kallas <eveliis.kallas@khk.ee>
2018-01-18 10:40:20 +0200

    HTML vaade faili sisu lugemine on kirjeldatud

--- model/template.php
+++ model/template.php
@@ -15,4 +15,12 @@ class template
     var $content = false;
     // reaalsed väärtused html vaade šablooni täitmiseks
     var $vars = array();
+    // template klassi meetodid
+    // html vaade faili sisu lugemine
+    function readFile($f){
+//        $fp = fopen($f, 'rb');
+//        $this->content = fread($fp, filesize($f));
+//        fclose($fp);
+        $this->content = file_get_contents($f);
+    }
 }
