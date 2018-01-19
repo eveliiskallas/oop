8698835c4db69e6c6dacea92d771e8e071c02c2f
eveliis.kallas <eveliis.kallas@khk.ee>
2018-01-19 14:48:35 +0200

    Reaalsed väärtused on määratud

--- index.php
+++ index.php
@@ -9,7 +9,14 @@
 require_once 'conf.php';
 
 $mainTmpl = new template('main');
-
+// reaalväärtuste määramine
+$mainTmpl->set('site_lang', 'et');
+$mainTmpl->set('site_title', 'PV');
+$mainTmpl->set('user', 'Kasutaja');
+$mainTmpl->set('title', 'Pealkiri');
+$mainTmpl->set('lang_bar', 'Keeleriba');
+$mainTmpl->set('menu', 'Lehe menüü');
+$mainTmpl->set('content', 'Lehe sisu');
 // väljastame objekti sisu test kujul
 echo'<pre>';
 print_r($mainTmpl);
