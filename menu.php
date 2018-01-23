76386132e6538fed1f1ecace6e0c3b15086181f3
eveliis.kallas <eveliis.kallas@khk.ee>
2018-01-23 14:24:16 +0200

    Ühe menüü elemendi lisamine on kirjutatud

--- menu.php
+++ menu.php
@@ -5,4 +5,20 @@
  * Date: 23.01.2018
  * Time: 14:12
  */
-$mainTmpl->set('menu', 'Lehe menüü');
+
+// loome menüü ehitamiseks vajalikud objektid
+$menuTmpl = new template('menu.menu'); // menüü mall
+$itemTmpl = new template('menu.item'); // menüü elemendi mall
+
+// loome üks menüü element nimega esimene
+
+$itemTmpl->set('name', 'esimene');
+// lisame antud elemendi menüüsse
+$menuItem = $itemTmpl->parsel(); // string, mis sisaldab ühe nimekirja elemendi lingiga
+$menuTmpl->set('menu_items', $menuItem); // nüüd olemas paar 'menu_items'=>'<li>...</li>'
+
+// ehitame valmis menüü
+$menu = $menuTmpl->parsel();
+
+// lisame valmis menüü lehele
+$mainTmpl->set('menu', $menu);
