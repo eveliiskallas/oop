b72827bc61c9d3d8816ef4ef84d7f9a436095e86
eveliis.kallas <eveliis.kallas@khk.ee>
2018-01-24 11:01:41 +0200

    Menüü on täiendatud linkidega

--- menu.php
+++ menu.php
@@ -11,13 +11,22 @@ $menuTmpl = new template('menu.menu'); // menüü mall
 $itemTmpl = new template('menu.item'); // menüü elemendi mall
 
 // loome üks menüü element nimega esimene
+//määrame menüüs väljastava elemendi nime
 $itemTmpl->set('name', 'esimene');
+// määrata menüüs väljastava elemendiga seotud link
+// http://ek.ikt.khk.ee/oop_vs17/index.php?control=esimene
+$link = $http->getLink(array('control'=>'esimene'));
+$itemTmpl->set('link', $link);
 // lisame antud elemendi menüüsse
 $menuItem = $itemTmpl->parsel(); // string, mis sisaldab ühe nimekirja elemendi lingiga
 $menuTmpl->add('menu_items', $menuItem); // nüüd olemas paar 'menu_items'=>'<li>...</li>'
 
 // loome veel ühe menüü element nimega esimene
 $itemTmpl->set('name', 'teine');
+// määrata menüüs väljastava elemendiga seotud link
+// http://ek.ikt.khk.ee/oop_vs17/index.php?control=esimene
+$link = $http->getLink(array('control'=>'teine'));
+$itemTmpl->set('link', $link);
 // lisame antud elemendi menüüsse
 $menuItem = $itemTmpl->parsel(); // string, mis sisaldab ühe nimekirja elemendi lingiga
 $menuTmpl->add('menu_items', $menuItem); // nüüd olemas paar 'menu_items'=>'<li>...</li>'
