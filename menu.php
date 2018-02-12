725a4a6831de5c7e9d8f8846380415b0f6df8618
eveliis.kallas <eveliis.kallas@khk.ee>
2018-02-12 10:23:22 +0200

    Koostati päring menüü ülesehitamiseks

--- menu.php
+++ menu.php
@@ -10,6 +10,17 @@
 $menuTmpl = new template('menu.menu'); // menüü mall
 $itemTmpl = new template('menu.item'); // menüü elemendi mall
 
+// koostame menüü ja sisu loomise päring
+$sql = 'SELECT content_id, content, title '.
+    'FROM content WHERE parent_id='.fixDB(0).
+    ' AND show_in_menu='.fixDB(1);
+$result = $db->getData($sql); // loeme andmed andmebaasist
+// vaatame testkujul tulemus
+echo '<pre>';
+print_r($result);
+echo '</pre>';
+
+
 // loome  ühe menüü element nimega avaleht
 $itemTmpl->set('name', 'avaleht');
 // määrata menüüs väljastava elemendiga seotud link
