b1d3279ec5e7407cc537b680cc47b6a1da935133
eveliis.kallas <eveliis.kallas@khk.ee>
2018-02-12 10:41:14 +0200

    Koostati vaikimisi kontrolleri lehtede vaadete loomist andmebaasi sisust

--- controllers/default.php
+++ controllers/default.php
@@ -5,4 +5,19 @@
  * Date: 31.01.2018
  * Time: 11:00
  */
-$mainTmpl->set('content', 'Vaikimisi sisu');
+$page_id = (int)$http->get('page_id'); // lehe id
+// koostame päringu contenti jaoks
+$sql = 'SELECT * FROM content '.
+    'WHERE content_id='.fixDB($page_id);
+// küsime andmed andmebaasist
+$result = $db->getData($sql);
+if($result == false){
+    $sql = 'SELECT * FROM content '.
+        'WHERE is_first_page='.fixDB(1);
+    $result = $db->getData($sql);
+}
+if($result != false){
+    $page = $result[0];
+    $http->set('page_id', $page['content_id']);
+    $mainTmpl->set('content', $page['content']);
+}
